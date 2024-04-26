b93351246d305b01b4e1a6c328abc0f1dedd5cd9
HittyGubby <70501834+HittyGubby@users.noreply.github.com>
2024-04-26 20:50:51 +0800

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,28 @@
+﻿#include <windows.h>
+#include <stdio.h>
+
+BOOL CALLBACK EnumWindowsProc(_In_ HWND hwnd, _In_ LPARAM Lparam)
+{
+	HWND hDefView = FindWindowEx(hwnd, 0, L"SHELLDLL_DefView", 0);
+	if (hDefView != 0) {
+		//find next class name workerw and hide
+		HWND hWorkerw = FindWindowEx(0, hwnd, L"WorkerW", 0);
+		ShowWindow(hWorkerw, SW_HIDE);
+
+		return FALSE;
+	}
+	return TRUE;
+}
+
+int main(int argc, char* argv[])
+{
+	{
+		HWND hProgman = FindWindow(L"Progman", 0);				// find pm	
+		SendMessageTimeout(hProgman, 0x52C, 0, 0, 0, 100, 0);	// send msg
+		HWND hTvn = FindWindow(L"TvnWindowClass", 0);				// find tvn window
+		SetParent(hTvn, hProgman);							// set as subwindow
+		EnumWindows(EnumWindowsProc, 0);						// find second workerw and hide
+	}
+
+	return 0;
+}
